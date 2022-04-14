a2b8b5a6e51a3cffc9c7252c0b4b2c02c0839a10
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-04-13 21:32:45 -0500

    new commit

--- script/cartoon.js
+++ script/cartoon.js
@@ -1,10 +1,12 @@
-const APIPath = "https://api.sampleapis.com/cartoons/cartoons2D";
-const numberOfResults = 9;
+const APIPath = "https://raw.githubusercontent.com/Tiemoue/new_project/main/content.json";
+const numberOfResults = 3;
 const cartoonGrid = document.querySelector('#cartoon-grid');
 const searchBtn = document.querySelector('#btn-search');
+const selectBtn = document.querySelector('#genre-option');
+const genreValue = document.querySelector('#genre-option').value;
 let cartoons = [];
 
-fetch("https://api.sampleapis.com/cartoons/cartoons2D")
+fetch("https://raw.githubusercontent.com/Tiemoue/new_project/main/content.json")
     .then(response => response.json())
     .then((data) => {
         cartoons = data;
@@ -44,11 +46,40 @@ function filterByName() {
         return cartoons;
     }
     console.log({searchInput});
+    console.log(filteredArray);
     return filteredArray;
 }
+
+function filterByGenre() {
+    const searchInput = document.querySelector('#genre-option').value;
+    let filteredArray = [];
+    if (searchInput === "Genre") {
+        return cartoons;
+    }
+    if(searchInput && searchInput.length){
+        cartoons.forEach((cartoon) => {
+            if(cartoon.genre
+                .includes(searchInput))
+                filteredArray.push(cartoon);
+        });
+    }else {
+        return cartoons;
+    }
+    console.log({searchInput});
+    return filteredArray;
+}
+
+
+
 
 searchBtn.addEventListener('click',(e)=>{
     console.log('Search button clicked...');
     let filteredList = filterByName();
+    updateCartoons(filteredList);
+});
+
+selectBtn.addEventListener('click',(e)=>{
+    console.log('Genre button clicked');
+    let filteredList = filterByGenre();
     updateCartoons(filteredList);
 });
